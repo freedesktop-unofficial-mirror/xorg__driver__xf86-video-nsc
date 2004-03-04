c706fe3f8f2a89fdb0ba16de8222531fcb246869
Kaleb Keithley <kaleb@freedesktop.org>
2004-03-04 20:16:28 +0000

    no bug report, see prior version. Fix bad edit

--- src/nsc_gx1_accel.c
+++ src/nsc_gx1_accel.c
@@ -1,4 +1,4 @@
-: /* $XdotOrg: xc/programs/Xserver/hw/xfree86/drivers/nsc/nsc_gx1_accel.c,v 1.8 2003/12/07 23:18:16 alanh Exp $ */
+/* $XdotOrg: xc/programs/Xserver/hw/xfree86/drivers/nsc/nsc_gx1_accel.c,v 1.1.4.3.4.1 2004/03/04 17:47:46 eich Exp $ */
 /* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/nsc/nsc_gx1_accel.c,v 1.7 2003/12/07 22:40:38 alanh Exp $ */
 /*
  * $Workfile: nsc_gx1_accel.c $
